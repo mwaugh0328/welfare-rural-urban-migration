534cbfe7488f35d1e00cfbc08bc502564fdffcb5
mw134 <mw134>
2021-11-04 10:17:43 -0400

    batch file

---
+++ appendix/appendix_2batch_file.m
@@ -0,0 +1,10 @@
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+disp('Rho = 0.0 calibration')
+
+run calibrate_fix_rho_wrap.m
+
+
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+disp('Ubar = 1 calibration')
+
+run calibrate_fix_ubar_wrap.m
