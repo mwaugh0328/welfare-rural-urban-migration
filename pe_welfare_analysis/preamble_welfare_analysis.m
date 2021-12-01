22d057f9d4ff80542556835b8cf8760b2258b88e
Waugh <irmew01@red.minneapolisfed.org>
2021-12-01 13:57:18 -0600

    now report all rural stats in pe analysis

--- pe_welfare_analysis/preamble_welfare_analysis.m
+++ pe_welfare_analysis/preamble_welfare_analysis.m
@@ -65,7 +65,7 @@ if isempty(specs)
     specs.follow_hh_expr = 11;  % number of time periods to follow a guy, historical was 11;
     specs.exp_index = [1,2,3,4,5,7,11]; % places to capture data (historical code was [1,2,3,4,5,7,11];
     
-    specs.Nmontecarlo = 30;
+    specs.Nmontecarlo = 5;
         
 end
 
