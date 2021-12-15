6fe02dab79fec0d849e84d6fa9cee68d9af1e23f
mikew <waugh.e.michael@gmail.com>
2021-12-15 13:42:43 -0500

    readme for bootstrap

--- bootstrap/run_montecarlo.m
+++ bootstrap/run_montecarlo.m
@@ -17,27 +17,27 @@ fval = zeros(Nruns,1);
 exitflag = zeros(Nruns,1);
 
 for xxx = 1:Nruns
-    
+
     disp(xxx)
-    
+
     [estimate(xxx,:), fval(xxx,1), exitflag(xxx,1)] = calibrate_montecarlo(fake_data(xxx,:));
-    
+
     disp(estimate(xxx,:))
-    
+
     disp(fval(xxx,1))
-    
+
     temp_estimate = estimate(1:xxx,:);
     temp_fval = fval(1:xxx,:);
     temp_exitflag = exitflag(1:xxx,:);
-   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     save montecarlo_edina estimate fval exitflag
     save('montecarlo_edina_estimate.csv', 'temp_estimate', '-ascii')
     save('montecarlo_edina_fval.csv', 'temp_fval', '-ascii')
     save('montecarlo_edina_exitflag.csv', 'temp_exitflag', '-ascii')
-    
-    system('git add montecarlo_edina.mat')
-    system('git add montecarlo_edina*.csv')
-    system('git commit -am "edina computer"')
-    system('git push')
-    
+
+    %system('git add montecarlo_edina.mat')
+    %system('git add montecarlo_edina*.csv')
+    %system('git commit -am "edina computer"')
+    %system('git push')
+
 end
